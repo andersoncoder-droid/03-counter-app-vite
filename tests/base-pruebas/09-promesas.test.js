b67386477db178445812807437f2cb4008487f9c
Anderson Losada Coder <andersonlosada.11@gmail.com>
2025-05-12 23:13:30 -0500

    Implement getHeroeByIdAsync function and add tests for asynchronous hero retrieval

---
+++ tests/base-pruebas/09-promesas.test.js
@@ -0,0 +1,15 @@
+import { getHeroeByIdAsync } from '../../src/base-pruebas/09-promesas';
+
+describe('Pruebas en 09-promesas', () => {
+  test('getHeroeByIdAsync debe de retornar un héroe', (done) => {
+    const id = 1;
+    getHeroeByIdAsync(id).then((hero) => {
+      expect(hero).toEqual({
+        id: 1,
+        name: 'Batman',
+        owner: 'DC',
+      });
+      done();
+    });
+  });
+});
